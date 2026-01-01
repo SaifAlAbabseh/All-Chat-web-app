e0ea0c1e8c94ef722d4d7a8590a79e37caebb04f
SaifAlAbabseh <sababseh97@gmail.com>
2026-01-01 10:59:38 +0300

    hotfix

--- mail.php
+++ mail.php
@@ -10,15 +10,14 @@ function sendFriendRequestMail($conn, $urlMainPath, $to_email, $to_name, $reques
     $site_name = "All Chat";
 
     $imageName = getUserImageName($conn, $requester_username);
-    $hostPath = generateHostPath($urlMainPath, $baseUrl);
-    $imagePath = $hostPath . "/Extra/styles/images/users_images/" . $imageName . ".png";
+    $imagePath = 'Extra/styles/images/users_images/' . $imageName . '.png';
 
     $body = str_replace(
-        ['{{REQUESTER_USERNAME}}', '{{YEAR}}', '{{SITE_NAME}}'],
-        [$requester_username, date('Y'), $site_name],
+        ['{{IMAGE_SOURCE}}', '{{REQUESTER_USERNAME}}', '{{YEAR}}', '{{SITE_NAME}}'],
+        [$imagePath, $requester_username, date('Y'), $site_name],
         $template
     );
-    return sendMail([true, $imagePath], $to_email, $to_name, $subject, $body);
+    return sendMail($to_email, $to_name, $subject, $body);
 }
 
 function sendVerificationCodeMail($to_email, $to_name, $code)
@@ -31,13 +30,13 @@ function sendVerificationCodeMail($to_email, $to_name, $code)
         [$code, date('Y'), $site_name],
         $template
     );
-    return sendMail([false, null], $to_email, $to_name, $subject, $body);
+    return sendMail($to_email, $to_name, $subject, $body);
 }
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-function sendMail($isImage, $to_email, $to_name, $subject, $body)
+function sendMail($to_email, $to_name, $subject, $body)
 {
 
     require 'mail/Exception.php';
@@ -49,7 +48,7 @@ function sendMail($isImage, $to_email, $to_name, $subject, $body)
 
     $mail = new PHPMailer;
     $mail->isSMTP();
-    $mail->SMTPDebug = 2; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
+    $mail->SMTPDebug = 0; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
     $mail->Host = "smtp.gmail.com"; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
     $mail->Port = 465; // TLS only
     $mail->SMTPSecure = 'ssl'; // ssl is deprecated
@@ -59,14 +58,8 @@ function sendMail($isImage, $to_email, $to_name, $subject, $body)
     $mail->setFrom($from_email, $from_name); // From email and name
     $mail->addAddress($to_email, $to_name); // to email and name
     $mail->Subject = $subject;
-    if ($isImage[0]) {
-        $mail->addEmbeddedImage($isImage[1], 'logo_cid');
-    }
-    $mail->isHTML(true);
-    $mail->Body = $body;
-    // $mail->msgHTML($body); //$mail->msgHTML(file_get_contents('contents.html'), __DIR__); //Read an HTML message body from an external file, convert referenced images to embedded,
+    $mail->msgHTML($body, __DIR__); //$mail->msgHTML(file_get_contents('contents.html'), __DIR__); //Read an HTML message body from an external file, convert referenced images to embedded,
     $mail->AltBody = 'HTML messaging not supported'; // If html emails is not supported by the receiver, show this body
-    // $mail->addAttachment('images/phpmailer_mini.png'); //Attach an image file
     $mail->SMTPOptions = array(
         'ssl' => array(
             'verify_peer' => false,
@@ -76,5 +69,3 @@ function sendMail($isImage, $to_email, $to_name, $subject, $body)
     );
     return $mail->send();
 }
-
-?>
