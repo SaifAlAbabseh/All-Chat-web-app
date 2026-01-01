f989afa5e0c513bf5f4d216e822ca4740c594a78
SaifAlAbabseh <sababseh97@gmail.com>
2026-01-01 06:07:46 +0300

    Hotfix..

--- Main/View Image/index.php
+++ Main/View Image/index.php
@@ -1,34 +1,45 @@
-<?php 
-session_start();
-if((isset($_REQUEST) && isset($_REQUEST["u"])) && 
-    (((isset($_SESSION) && isset($_SESSION["who"]) && isset($_REQUEST))) || 
-    (isset($_REQUEST["token"]) && isset($_REQUEST["token"]) != ""))) {
+<?php
+
+if (isset($_REQUEST) && isset($_REQUEST["u"])) {
+    if (isset($_REQUEST["token"])) {
+        if ($_REQUEST["token"] == "")
+            die("Not valid token..");
         require_once("../../DB.php");
-        if(isset($_REQUEST["token"])) {
-            if($_REQUEST["token"] == "")
-                die("Not valid token..");
-            $token = $_REQUEST["token"];
-            $query = "SELECT * FROM users WHERE view_image_token='".$token."'";
-            $result = mysqli_query($conn, $query);
-            if(!($result && mysqli_num_rows($result) > 0)) {
-                die("Not valid token..");
-            }
+        $token = $_REQUEST["token"];
+        $query = "SELECT * FROM users WHERE view_image_token='" . $token . "'";
+        $result = mysqli_query($conn, $query);
+        if (!($result && mysqli_num_rows($result) > 0)) {
+            mysqli_close($conn);
+            die("Not valid token..");
         }
-        $query="SELECT picture FROM users WHERE BINARY username='".$_REQUEST["u"]."'";
-        $result=mysqli_query($conn,$query);
-        mysqli_close($conn);
-        $temp;
-        if($result){
-            if(mysqli_num_rows($result)){
-                $row=mysqli_fetch_row($result);
-                $temp="".$row[0];
-            }
+        getImage($conn, $_REQUEST["u"]);
+    } else {
+        session_start();
+        if (isset($_SESSION) && isset($_SESSION["who"])) {
+            require_once("../../DB.php");
+            getImage($conn, $_REQUEST["u"]);
+        } else {
+            die("Not authorized..");
         }
-        $path="../../Extra/styles/images/users images/".$temp.".png";
-        header('Content-Type: image/png');
-        readfile($path);
+    }
+} else {
+    die("Error..");
 }
-else{
-    die("Not authorized..");
+
+function getImage($conn, $username) {
+    $query = "SELECT picture FROM users WHERE BINARY username='" . $username . "'";
+    $result = mysqli_query($conn, $query);
+    mysqli_close($conn);
+    $temp = "user";
+    if ($result) {
+        if (mysqli_num_rows($result)) {
+            $row = mysqli_fetch_row($result);
+            $temp = $row[0];
+        }
+    }
+    $path = "../../Extra/styles/images/users images/" . $temp . ".png";
+    header('Content-Type: image/png');
+    readfile($path);
 }
+
 ?>
