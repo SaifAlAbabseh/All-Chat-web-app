0488f2eb39151f2a23883bf4bd9a338b7ab55ce3
SaifAlAbabseh <sababseh97@gmail.com>
2026-03-06 05:54:55 +0300

    Hotfix..

--- Mobile/createAccount.php
+++ Mobile/createAccount.php
@@ -2,20 +2,7 @@
 
 require_once "../DB.php";
 
-if (isset($_REQUEST) && isset($_REQUEST["check"]) && $_REQUEST["check"] == "fromMobile1090" && isset($_REQUEST["username"]) && isset($_REQUEST["password"])) {
-    $you = $_REQUEST["username"];
-    $password = $_REQUEST["password"];
-
-    $check_query = "SELECT * FROM users WHERE BINARY username=? AND password=?";
-    $stmt = mysqli_prepare($conn, $check_query);
-    mysqli_stmt_bind_param($stmt, "ss", $you, $password);
-    mysqli_stmt_execute($stmt);
-    $check_result = mysqli_stmt_get_result($stmt);
-    if (mysqli_num_rows($check_result) == 0) {
-        header("Location:../index.php");
-    }
-}
-else {
+if (!(isset($_REQUEST) && isset($_REQUEST["check"]) && $_REQUEST["check"] == "fromMobile1090")) {
     header("Location:../index.php");
 }
 
