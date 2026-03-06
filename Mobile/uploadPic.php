a12843004bd1d6b7019a7ed0f8894c678f1acbf2
SaifAlAbabseh <sababseh97@gmail.com>
2026-03-06 06:04:52 +0300

    Hotfix

--- Mobile/uploadPic.php
+++ Mobile/uploadPic.php
@@ -28,7 +28,7 @@ else{
         ALL CHAT
     </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= asset('Extra/styles/cssFiles/themes.css') ?>" />
+    <link rel="stylesheet" href="<?= asset('../Extra/styles/cssFiles/themes.css') ?>" />
     <style>
         body,
         html {
@@ -42,7 +42,7 @@ else{
             align-items: center;
         }
     </style>
-    <script src="<?= asset('scripts/commonMethods.js') ?>"></script>
+    <script src="<?= asset('../scripts/commonMethods.js') ?>"></script>
 </head>
 
 <body>
